834b1bb86054703d4a97cb921b48a3f5f1b01d4f
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-07-27 12:45:07 +0100

    Turn off vanilla tests
    Apparently --skip-test-unit was insufficient or wrong

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module PolitAds
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework  :rspec, fixture: false
+      g.view_specs      false
+      g.helper_specs    false
+    end
   end
 end
