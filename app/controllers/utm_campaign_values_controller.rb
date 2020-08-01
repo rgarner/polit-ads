c97b3d568e9db3e7aa1fc37c6e6d73d9cbef2c97
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-08-01 14:52:54 +0100

    Add utm_campaign_values/against URL
    No tests, still.

--- app/controllers/utm_campaign_values_controller.rb
+++ app/controllers/utm_campaign_values_controller.rb
@@ -11,4 +11,11 @@ class UtmCampaignValuesController < ApplicationController
     @index = params[:id]
     @values = UtmCampaignValue.select('utm_campaign_values.value, COUNT(*) AS count').where(index: @index).group(:value)
   end
+
+  def against
+    @table = UtmCampaignValue::ContingencyTable.new(
+      params[:utm_campaign_value_id],
+      params[:other_id]
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :adverts, only: :show
   get 'adverts/by_utm_value/:utm_value', to: 'adverts#by_utm_value', as: :adverts_by_utm_value
-  resources :utm_campaign_values, only: %i[show index]
+  resources :utm_campaign_values, only: %i[show index] do
+    get 'against/:other_id', to: 'utm_campaign_values#against'
+  end
 end
