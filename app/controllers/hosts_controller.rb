09ac2703f7a9294fc0a176ec78ad9149dd1691fa
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-08-05 19:32:37 +0100

    Breadcrumb up hosts
    Also add first and last sightings of ads for host

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,10 +1,14 @@
 class HostsController < ApplicationController
+  breadcrumb 'Hosts', :hosts_path, match: :exclusive
+
   def index
     @hosts = Host.with_ad_counts
   end
 
   def adverts
     @host = Host.find(params[:host_id])
-    @adverts = @host.adverts.page(params[:page])
+    @adverts = @host.adverts.order(ad_creation_time: :desc).page(params[:page])
+
+    breadcrumb "#{@host.hostname} / Adverts", request.path
   end
 end
--- app/models/host.rb
+++ app/models/host.rb
@@ -2,7 +2,8 @@ class Host < ActiveRecord::Base
   has_many :adverts
 
   scope :with_ad_counts, lambda {
-    select('hosts.*, COUNT(adverts.id) AS ad_count')
+    select('hosts.*, MIN(adverts.ad_creation_time) AS ad_first, '\
+           'MAX(adverts.ad_creation_time) AS ad_last, COUNT(adverts.id) AS ad_count')
       .joins(:adverts)
       .group('hosts.id')
       .order('COUNT(adverts.id) DESC')
