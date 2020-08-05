9f90927fa1ba0af32a654e8f293afb3236216942
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-08-05 13:43:31 +0100

    Turn off bits of Rails
    ActiveStorage and ActionMailbox are spewing routes, and they're
    just hampering readability. Take the requires from rails/all and
    delete them

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,20 @@
 require_relative 'boot'
 
-require 'rails/all'
-
+%w(
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_cable/engine
+  rails/test_unit/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
