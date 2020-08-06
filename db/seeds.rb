79c1cb69937aadcbb31fedd805999f25ccd0419e
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-08-06 12:44:29 +0100

    Seeds for campaigns/funders
    Not tied into scraper yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+biden = Campaign.where(name: 'Joe Biden', slug: 'biden').first_or_create!
+trump = Campaign.where(name: 'Donald J. Trump', slug: 'trump').first_or_create!
+
+[
+  'TRUMP MAKE AMERICA GREAT AGAIN COMMITTEE',
+  'DONALD J. TRUMP FOR PRESIDENT, INC.'
+].each do |entity_name|
+  trump.funding_entities.where(name: entity_name).first_or_create!
+end
+
+[
+  'BIDEN FOR PRESIDENT',
+  'BIDEN VICTORY FUND',
+  'Biden for President'
+].each do |entity_name|
+  biden.funding_entities.where(name: entity_name).first_or_create!
+end
