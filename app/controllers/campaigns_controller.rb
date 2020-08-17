6adb27966f7d9cb0d6db5d4955fbb4e7809b9266
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-08-17 14:12:07 +0100

    Responsive images on /campaigns
    They size and collapse properly now

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,7 +2,7 @@ class CampaignsController < ApplicationController
   ADS_SINCE = '2020-07-01'.freeze
 
   def index
-    @ads_since = ADS_SINCE
+    @ads_since = Date.parse(ADS_SINCE)
     @campaigns = Campaign.with_summaries.since(@ads_since)
     @ad_counts = Campaign.summary_graph_data
     @funding_entity_count = FundingEntity.count
--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -2,4 +2,11 @@ module CampaignsHelper
   def advert_count_text
     "#{number_with_delimiter(@campaigns.map(&:ad_count).reduce(&:+))} adverts"
   end
+
+  def campaign_face(slug)
+    image_tag(
+      asset_path("face-circle-#{slug}-700x700.png"),
+      class: 'face img-fluid'
+    )
+  end
 end
