a1e42e53b3a2b030c94e60445d3fc4268ec33833
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-08-27 13:42:16 +0300

    Show ad code value description if exists
    It's a bit nasty, join-wise; probably AdCodeValueSummaries should cease to be
    a MATERIALIZED VIEW and should fold into AdCodeValueDescriptions and be renamed
    AdCodeValues (leaving room for AdCodeValueUsages to replace UtmCampaignValues)?

--- app/controllers/values_controller.rb
+++ app/controllers/values_controller.rb
@@ -3,20 +3,16 @@ class ValuesController < ApplicationController
   breadcrumb 'Ad codes', -> { campaign_ad_codes_path(campaign_id: 'trump') }, match: :exclusive
 
   def show
-    @value = AdCodeValueSummary.joins(:campaign).find_by!(
-      'campaigns.slug' => params[:campaign_id],
-      index: params[:ad_code_id],
-      value: params[:id]
+    @value = AdCodeValueSummary.joins(:campaign).find_by(
+      'campaigns.slug' => params[:campaign_id], index: params[:ad_code_id], value: params[:id]
+    )
+    @description = AdCodeValueDescription.joins(ad_code: :campaign).find_by(
+      'campaigns.slug' => params[:campaign_id], 'ad_codes.index' => params[:ad_code_id], value: params[:id]
     )
 
-    @values = AdCodeValueSummary.between(
-      @value.index,
-      @value.value,
-      start,
-      finish
-    )
+    @values = AdCodeValueSummary.between(@value.index, @value.value, start, finish)
 
-    breadcrumb @value.name, request.path # no AdCode path at time of writing
+    breadcrumb @value.name, campaign_ad_code_path(params[:campaign_id], @value.index), match: :exclusive
     breadcrumb @value.value, request.path
   end
 
