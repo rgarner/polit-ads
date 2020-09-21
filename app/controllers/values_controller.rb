98c2e1bbdeab0f23dbb771480ad466a0aed22602
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-09-21 13:42:15 +0100

    Make values#show campaign-aware
    Was fixed to Trump

--- app/controllers/values_controller.rb
+++ app/controllers/values_controller.rb
@@ -1,7 +1,4 @@
 class ValuesController < ApplicationController
-  breadcrumb 'Donald J. Trump', :current
-  breadcrumb 'Ad codes', -> { campaign_ad_codes_path(campaign_id: 'trump') }, match: :exclusive
-
   def show
     @value = AdCodeValueSummary.joins(:campaign).find_by(
       'campaigns.slug' => params[:campaign_id], index: params[:ad_code_id], value: params[:id]
@@ -12,8 +9,7 @@ class ValuesController < ApplicationController
 
     @values = AdCodeValueSummary.between(@value.index, @value.value, start, finish)
 
-    breadcrumb @value.name, campaign_ad_code_path(params[:campaign_id], @value.index), match: :exclusive
-    breadcrumb @value.value, request.path
+    set_breadcrumbs!
   end
 
   def start
@@ -22,5 +18,14 @@ class ValuesController < ApplicationController
 
   def finish
     @finish ||= params[:end] ? Date.parse(params[:end]) : Date.today
+  end
+
+  private
+
+  def set_breadcrumbs!
+    breadcrumb @value.campaign.name, :current
+    breadcrumb 'Ad codes', campaign_ad_codes_path(@value.campaign), match: :exclusive
+    breadcrumb @value.name, campaign_ad_code_path(params[:campaign_id], @value.index), match: :exclusive
+    breadcrumb @value.value, request.path
   end
 end
