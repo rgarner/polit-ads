569e14430ef27e7677686de2fd5cbad40534f314
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-10-06 16:53:19 +0100

    Add spend dimension to values#show
    Use a shared/dimension_dropdown partial

--- app/models/ad_code_daily_summary.rb
+++ app/models/ad_code_daily_summary.rb
@@ -8,7 +8,7 @@ class AdCodeDailySummary < ApplicationRecord
   ##
   # Return a list of hashes (only one!) in a form required by Chartkick,
   # e.g. { name: 'djt', data: { '2020-08-04' => 43, '2020-08-05' => 17 }  }
-  def self.between(index, value, start, finish)
+  def self.between(index, value, start, finish, dimension: 'count')
     start = start.strftime('%Y-%m-%d')
     finish = finish.strftime('%Y-%m-%d')
 
@@ -16,12 +16,12 @@ class AdCodeDailySummary < ApplicationRecord
       BETWEEN_SQL, 'sql', [[nil, start], [nil, finish], [nil, index], [nil, value]]
     )
 
-    group_for_chartkick(result)
+    group_for_chartkick(result, dimension: dimension)
   end
 
   BETWEEN_SQL =
     <<~SQL.freeze
-      SELECT start, count
+      SELECT start, count, approximate_spend
       FROM ad_code_daily_summaries
       WHERE start BETWEEN $1 AND $2
       AND index = $3 AND value = $4
