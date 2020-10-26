2760c1f0ca6d5400d9bbbcbba6323b5bb0ba517b
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-10-26 08:43:46 +0000

    Remap elv values at utm1 and utm14
    Sometimes instead of values like 'nd' and 'na', in utm1 and utm14
    we see values like:
    
    https://vote.donaldjtrump.com/?gotvelvia
    and
    https://vote.donaldjtrump.com/?lp0240
    
    These are carrying the real values for ad campaign and ad LP code,
    and we can assume the values at their respective positions to be the broadest
    'na'.

--- lib/polit_ads/utm_campaign_splitter.rb
+++ lib/polit_ads/utm_campaign_splitter.rb
@@ -27,9 +27,56 @@ module PolitAds
 
     private
 
+    ##
+    # Normalize for any shonky values
+    class TrumpUtmNormalizer
+      URL_CARRYING_A_VALUE = %r{https://vote\.donaldjtrump\.com/\?(?<value>.*)}.freeze
+
+      attr_reader :utm_values
+
+      def initialize(utm_values)
+        @utm_values = utm_values.split('_')
+      end
+
+      def elv_urls_at_1_and_14?
+        utm_values[1] =~ URL_CARRYING_A_VALUE && utm_values[14] =~ URL_CARRYING_A_VALUE
+      end
+
+      def value_carried_in_url(index)
+        match = utm_values[index].match(URL_CARRYING_A_VALUE)
+        match[:value]
+      end
+
+      # @return [Array<String>]
+      def remap_elv_values
+        ad_campaign = value_carried_in_url(1)
+        ad_lp_code = value_carried_in_url(14)
+
+        [
+          utm_values[0],
+          'na',
+          ad_campaign,
+          *utm_values[2..13],
+          'na',
+          ad_lp_code,
+          *utm_values[15..]
+        ]
+      end
+
+      def to_a
+        if elv_urls_at_1_and_14?
+          remap_elv_values
+        else
+          utm_values
+        end
+      end
+    end
+
     def utm_campaign_values(advert)
       url = Addressable::URI.parse(advert.external_url)
-      url.query_values['utm_campaign'].split('_')
+
+      normalizer = TrumpUtmNormalizer.new(url.query_values['utm_campaign'])
+      normalizer.to_a
     end
   end
 end
